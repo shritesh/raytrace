649ea4b9011779219598afc1f442fbf2ff2a8d35
Shritesh Bhattarai <shr@ite.sh>
2020-05-05 13:24:26 -0500

    Chapter 5

--- raytrace.jl
+++ raytrace.jl
@@ -12,13 +12,26 @@ const horizontal = Vec3(4, 0, 0)
 const vertical = Vec3(0, 2.25, 0)
 const lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, 1)
 
-function ray_color(ray::Ray)
-   unit_direction = normalize(ray.direction)
-   t = 0.5 * (unit_direction[2] + 1)
-   (1 - t) * Vec3(1, 1, 1) + t * Vec3(0.5, 0.7, 1)
+function hit_sphere(center::Vec3, radius::Float64, r::Ray)::Bool
+   oc = r.origin - center
+   a = dot(r.direction, r.direction)
+   b = 2 * dot(oc, r.direction)
+   c = dot(oc, oc) - radius * radius
+   discriminant = b * b - 4 * a * c
+   discriminant > 0
 end
 
-function color(i::Int, j::Int)
+function ray_color(r::Ray)::Vec3
+   if hit_sphere(Vec3(0, 0, -1), 0.5, r)
+      Vec3(1, 0, 0)
+   else
+      unit_direction = normalize(r.direction)
+      t = 0.5 * (unit_direction[2] + 1)
+      (1 - t) * Vec3(1, 1, 1) + t * Vec3(0.5, 0.7, 1)
+   end
+end
+
+function color(i::Int, j::Int)::Vec3
    u = i / image_width
    v = j / image_height
 
