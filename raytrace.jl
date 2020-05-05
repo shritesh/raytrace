e675d7106e8d18711bfefa19b45b1be3db4eda2b
Shritesh Bhattarai <shr@ite.sh>
2020-05-05 12:31:57 -0500

    Remove ProgressMeter as it kills performance

--- raytrace.jl
+++ raytrace.jl
@@ -1,20 +1,14 @@
-using Images, ProgressMeter
-
-include("vec3.jl")
-
+using Images
 
 const image_width = 256
 const image_height = 256
 
 function color(i, j)
-    Vec3(i / image_width, j / image_height, 0.25)
+    (i / image_width, j / image_height, 0.25)
 end
 
 function render()
-    @showprogress map(
-        coords -> RGB(color(coords...)...),
-        [(i, j) for j = image_height:-1:1, i = 1:image_width],
-    )
+    [RGB(color(i, j)...) for j = image_height:-1:1, i = 1:image_width]
 end
 
-render()
+@time render()
